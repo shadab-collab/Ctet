500a7af798fb4090cde1af3d8523cd3a6ce1c68a
shadab-collab <shadabjain.ss@gmail.com>
2026-07-22 11:22:33 +0530

    update

--- server.js
+++ server.js
@@ -8,23 +8,19 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// HTML, CSS, JS सर्व करें
-app.use(express.static(__dirname));
+// Public Folder
+app.use(express.static(path.join(__dirname, "public")));
 
-// MongoDB
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
-// API
 app.use("/api/results", require("./routes/results"));
 
-// Home Page
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-// Render PORT
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
