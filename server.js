0e93703b921d74ea93bbce7ba261836be93e8de5
shadab-collab <shadabjain.ss@gmail.com>
2026-07-22 11:16:16 +0530

    update server.js

--- server.js
+++ server.js
@@ -1,22 +1,31 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
+// HTML, CSS, JS सर्व करें
+app.use(express.static(__dirname));
+
+// MongoDB
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
+// API
 app.use("/api/results", require("./routes/results"));
 
-const PORT = 3000;
+// Home Page
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// Render PORT
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log("Server Running on Port " + PORT);
